03c35b6dad6eb14f4a2b57f3d0418d46372f9ec5
Rajendraprasad02 <rajendra.prasad@talentakeaways.com>
2025-03-11 10:21:54 +0530

    hash routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,21 @@
 import React from "react";
 import "./App.css";
 import Login from "./components/login/Login";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import Signup from "./components/login/Signup";
 import InvoiceEmails from "./components/invoice/InvoiceEmails";
 import SetPassword from "./components/login/SetPassword";
 
+const router = createHashRouter([
+  { path: "/", element: <Login /> },
+  { path: "/signup", element: <Signup /> },
+  { path: "/invoice-emails", element: <InvoiceEmails /> },
+  { path: "/set-password", element: <SetPassword /> },
+  { path: "/set", element: <Signup /> },
+]);
+
 function App() {
-  return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/invoice-emails" element={<InvoiceEmails />} />
-          <Route path="/set-password" element={<SetPassword />} />
-          <Route path="/set" element={<Signup />} />
-        </Routes>
-      </Router>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
