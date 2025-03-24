c290f57a72421b12beb307afb55f3dc8c9be20e4
Rajendraprasad02 <rajendra.prasad@talentakeaways.com>
2025-03-24 18:43:09 +0530

    bill payment - 34

--- src/components/billPayment/BillPayment.jsx
+++ src/components/billPayment/BillPayment.jsx
@@ -110,7 +110,7 @@ const BillPayment = () => {
     }
   };
 
-  if (!paymentLoader)
+  if (paymentLoader)
     return (
       <div className="flex flex-col items-center justify-center h-screen w-full bg-zinc-950">
         <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500"></div>
